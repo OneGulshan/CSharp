59e186a319685154a17ac0e99b99d8d1ba8d743a
OneGulshan <110595038+OneGulshan@users.noreply.github.com>
2023-08-07 15:21:28 +0530

    Update MyStaticClass.cs

--- CSharp/MyStaticClass.cs
+++ CSharp/MyStaticClass.cs
@@ -1,21 +1,23 @@
-﻿﻿usingSystem;
-usingstaticCSharp.Program;
+using System;
+using static CSharp.Program;
 
-namespaceCSharp
+namespace CSharp
 {
-staticclassMyStaticClass
-{
-publicstaticvoidFunc2(thisExtensionClassec)//Thisisanextensionmethodnoneedtoprovidevaluetothisparameter//thisextensionmethodparameteriscalledbindingparametersthatsbelongstoExtensionClassspecifiedhere
-{
-Console.WriteLine("Thisisthirdmethod..");
-}
+    static class MyStaticClass
+    {
+        public static void Func2(this ExtensionClass ec)//This is an extension method no need to provide value to this parameter//this extension method parameter is called binding parameters thats belongs to ExtensionClass specified here
+        {
+            Console.WriteLine("This is third method..");
+        }
 
-publicstaticvoidFunc3(thisExtensionClassec,inta)//inta<-thisisannormalparameternotignoredoncallingtime
-{
-Console.WriteLine("Thisisthirdmethod.."+a);
-}publicstaticboolIsGreaterThan(thisinti,intvalue)
-{
-returni>value;
-}
-}
+        public static void Func3(this ExtensionClass ec, int a)//int a <- this is an normal parameter not ignored on calling time
+        {
+            Console.WriteLine("This is third method.. " + a);
+        }
+
+        public static bool IsGreaterThan(this int i, int value)
+        {
+            return i > value;
+        }
+    }
 }
